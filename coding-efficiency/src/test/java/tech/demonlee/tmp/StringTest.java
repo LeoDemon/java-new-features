6859637eb41e437fb3f2d24cd27545610c0a1251
Demon.Lee <jwkljh@aliyun.com>
2023-08-16 14:04:31 +0800

    test(file): test cases for String and Line Break

--- coding-efficiency/src/test/java/tech/demonlee/tmp/StringTest.java
+++ coding-efficiency/src/test/java/tech/demonlee/tmp/StringTest.java
@@ -1,8 +1,31 @@
 package tech.demonlee.tmp;
 
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
 /**
  * @author Demon.Lee
  * @date 2023-06-12 16:36
  */
+@Log4j2
 public class StringTest {
+
+    @Test
+    public void testIntegerConvert() {
+        Integer aa = null;
+        String aaa = Objects.toString(aa, null);
+        System.out.println("aaa: " + aaa);
+
+        aa = 30;
+        aaa = Objects.toString(aa, null);
+        System.out.println("aaa: " + aaa);
+    }
+
+    @Test
+    public void testLineBreak() {
+        String lineBreak = System.getProperty("line.separator");
+        log.info("lineBreak:[{}]", lineBreak);
+    }
 }
