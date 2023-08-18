d9402f129a092218f36dc2092f66a98718acce08
Demon.Lee <jwkljh@aliyun.com>
2023-08-18 15:05:37 +0800

    test(class): test cases for Class name

---
+++ coding-efficiency/src/test/java/tech/demonlee/tmp/ClassTest.java
@@ -0,0 +1,32 @@
+package tech.demonlee.tmp;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+/**
+ * @author Demon.Lee
+ * @date 2023-08-18 14:58
+ */
+@Log4j2
+public class ClassTest {
+
+    @Test
+    public void testClassName() {
+        displayClassName(this.getClass());
+        displayClassName(int.class);
+        displayClassName(Integer.class);
+        displayClassName(List.class);
+        displayClassName(String[].class);
+    }
+
+    private void displayClassName(Class<?> clazz) {
+        log.info("name: " + clazz.getName());
+        log.info("simple name: " + clazz.getSimpleName());
+        log.info("canonical name: " + clazz.getCanonicalName());
+        log.info("type name: " + clazz.getTypeName());
+        log.info("package name: " + clazz.getPackageName());
+        log.info("----------------------");
+    }
+}
